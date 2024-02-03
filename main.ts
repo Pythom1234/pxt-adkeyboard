b06a7eedf2635704c6eb9a3eda928f3144bb2ea8
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-02-03 10:33:46 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -11,18 +11,40 @@ namespace ADKeyboard {
     //% weight=99
     //% color=#00f000
     //% block.loc.cs="klávesa $k je stisknuta na ADKeyboard na pinu %p"
-    export function ADKeyboard(k: ADKeys, p: AnalogPin): boolean {
+    export function adKeyboardIsPressed(k: ADKeys, p: AnalogPin): boolean {
         let a: number = pins.analogReadPin(p);
         if (a < 10 && k == 1) {
-            return true;
+            return true
         } else if (a >= 40 && a <= 60 && k == 2) {
-            return true;
+            return true
         } else if (a >= 80 && a <= 110 && k == 3) {
-            return true;
+            return true
         } else if (a >= 130 && a <= 150 && k == 4) {
-            return true;
+            return true
         } else if (a >= 530 && a <= 580 && k == 5) {
-            return true;
-        } else return false;
+            return true
+        } else{
+            return false
+        }
+    }
+    //% block="key pressed on ADKeyboard at pin %p"
+    //% weight=98
+    //% color=#00f000
+    //% block.loc.cs="klávesa stisknuta na ADKeyboard na pinu %p"
+    export function adKeyboardGetPressed(p: AnalogPin): string {
+        let a: number = pins.analogReadPin(p);
+        if (a < 10) {
+            return "A"
+        } else if (a >= 40 && a <= 60) {
+            return "B"
+        } else if (a >= 80 && a <= 110) {
+            return "C"
+        } else if (a >= 130 && a <= 150) {
+            return "D"
+        } else if (a >= 530 && a <= 580) {
+            return "E"
+        } else {
+            return ""
+        }
     }
 }
